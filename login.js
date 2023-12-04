63ac0f444e1373035fa38dedc0771d6ad2d8ea34
nguyenkhanhduysdtq <nguyenkhanhduy11103@gmail.com>
2023-12-05 00:47:35 +0700

    add file

--- login.js
+++ login.js
@@ -4,6 +4,9 @@ const admin = {
     userAdmin: "duysdtq",
     passwordAdmin: "123456",
 }
+// khai bào mảng các tài khoản người dùng
+
+var userAccount = [];
 
 
 
@@ -13,6 +16,23 @@ var user = JSON.parse(localStorage.getItem("userOnline"));
 if (user.userAdmin == admin.userAdmin && user.passwordAdmin == admin.passwordAdmin) {
     var titleAdmin = document.getElementById("CheckQuestion");
     titleAdmin.innerHTML = '<i class="fa-solid fa-clipboard-question"></i>' + "Xem câu hỏi";
+}
+
+
+
+function checkAccountExist(username, password) {
+    var userExist = [];
+
+    userExist = JSON.parse(localStorage.getItem("userAccount"));
+
+    for (var i = 0; i < userExist.length; i++) {
+        if (username == userExist[i].username && password == userExist[i].password) {
+            return i;
+        }
+    }
+
+    return -1;
+
 }
 
 
@@ -53,6 +73,24 @@ function checkAccount() {
 
         closeLogin();
 
+    } else if (checkAccountExist(username.value, password.value) != -1) {
+
+        var index = checkAccountExist(username.value, password.value);
+        var userExist = [];
+
+        userExist = JSON.parse(localStorage.getItem("userAccount"));
+
+        localStorage.setItem("userOnline", JSON.stringify(userExist[index]));
+        var nameUser = document.getElementById("nameUser");
+
+        nameUser.innerHTML = '<i class="fa-solid fa-user-tag"></i>' + userExist[index].name;
+
+        alert("Bạn đã đăng nhập thành công ,");
+
+        closeLogin();
+
+
+
     } else {
         alert("Bạn đăng nhập không thành công");
     }
@@ -64,11 +102,71 @@ function checkAccount() {
 
 
 function check() {
-    if (localStorage.getItem("userOnline") == null) {
+    if (JSON.parse(localStorage.getItem("userOnline")) == null) {
         alert("Bạn chưa đăng nhập");
     } else {
-        alert("Bạn đang chuyển trang");
+        window.location.href = "homepage.html";
     }
+}
+
+
+
+function outLogin() {
+    localStorage.setItem("userOnline", null);
+    alert("Bạn đã dăng xuất ");
+    window.location.href = "loginwed.html";
+
+
+
+}
+
+function register() {
+    var form_register = document.getElementById("form-register");
+    var form_login = document.getElementById("form-login");
+
+    form_login.classList.add("active");
+
+    form_register.classList.add("active");
+
+
+}
+
+
+function closeRegister() {
+    var form_register = document.getElementById("form-register");
+    var form_login = document.getElementById("form-login");
+
+    form_login.classList.remove("active");
+
+    form_register.classList.remove("active");
+}
+
+
+
+function checkRegister() {
+    var name = document.getElementById("name-account");
+    var usernname = document.getElementById("username-register");
+    var password = document.getElementById("password-register");
+    var password2 = document.getElementById("password-commit-register");
+
+    var user_register = {
+        name: name.value,
+        username: usernname.value,
+        password: password.value,
+    }
+    userAccount.push(user_register);
+
+    localStorage.setItem("userAccount", JSON.stringify(userAccount));
+    alert("Bạn đăng ký tài khoản thành công");
+
+    name.value = "";
+    usernname.value = "";
+    password.value = "";
+    password2.value = "";
+
+    closeRegister();
+
+
 }
 
 
